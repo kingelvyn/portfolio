572acf71d16a2c4e44768b2f88e0d7fcd7c45b65
kingelvyn <elvyncachapero@gmail.com>
2025-05-08 06:56:38 -0500

    add basic html setup

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
-package portfolio
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	// Serve main page
+	http.HandleFunc("/", handler)
+
+	// Serve static files
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static", http.StripPrefix("/static", fs))
+
+	log.Println("Server starting @ :3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
